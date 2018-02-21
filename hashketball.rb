cbdaaa7c589c0455f69ac6c383fc0af8c503c8d2
Harim Choi <harim1206@gmail.com>
2018-02-21 13:50:02 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,8 @@ def num_points_scored(player_name)
           |player, statistic|
 
           if player == player_name
-          binding.pry
+            return player[points]
+          end
         }
 
       end
